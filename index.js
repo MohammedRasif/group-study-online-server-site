3d8809299b481525751e97b7c1a0b47363c614b8
md rasif <mdrasif3499@gmail.com>
2024-06-26 01:51:59 +0600

    final code push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -7,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-  origin:['http://localhost:5173'
+  origin:['http://localhost:5174'
     ,'https://group-study-d6cf6.web.app','https://group-study-d6cf6.firebaseapp.com'
   ],
       credentials:true,
